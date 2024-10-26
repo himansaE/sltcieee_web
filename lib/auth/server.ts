d61d566780b4f1258058d9ab0238bb714c55b024
Himansa Eshan <41321171+himansaE@users.noreply.github.com>
2024-10-27 02:27:35 +0530

    ref: authentication on client

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "./prisma";
+import prisma from "../prisma";
 import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
